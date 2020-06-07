7f924eff7eb3a52d37bacb580f31d356a61b5dec
reza <rezashoja13g@gmail.com>
2020-06-07 18:58:36 +0430

    Updates

---
+++ precache-manifest.baf9c080f73f149ab9a8bcff4662154b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94521982eba20c481de3d30d56d6b109",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "ac3401fde081d8b93e8b",
+    "url": "/test-app/static/css/main.02f10bc0.chunk.css"
+  },
+  {
+    "revision": "e28a07d93a3698580ce5",
+    "url": "/test-app/static/js/2.d04f0247.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app/static/js/2.d04f0247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3401fde081d8b93e8b",
+    "url": "/test-app/static/js/main.1acf8244.chunk.js"
+  },
+  {
+    "revision": "1c3ac668049c0d68a235",
+    "url": "/test-app/static/js/runtime-main.83806e2b.js"
+  },
+  {
+    "revision": "64ca5b5ad6d52e9449d6881908918564",
+    "url": "/test-app/static/media/basket.64ca5b5a.svg"
+  },
+  {
+    "revision": "7ba705fda5d2decb523204d25d9565bd",
+    "url": "/test-app/static/media/discount.7ba705fd.svg"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/test-app/static/media/kitchen.44dbaa78.svg"
+  },
+  {
+    "revision": "69b30251c6aa797f09b4e1938c3fd6dc",
+    "url": "/test-app/static/media/minus.69b30251.svg"
+  },
+  {
+    "revision": "9b08278346507cef320f9f9556af5c09",
+    "url": "/test-app/static/media/plus.9b082783.svg"
+  },
+  {
+    "revision": "c412776699c042548bfc30e7b2fa85ca",
+    "url": "/test-app/static/media/search.c4127766.svg"
+  },
+  {
+    "revision": "2e87fe64fa5e0a535863e1b193e52f21",
+    "url": "/test-app/static/media/tea.2e87fe64.svg"
+  },
+  {
+    "revision": "d4e9b030bffccb5bc65c55f99c87fc84",
+    "url": "/test-app/static/media/trash.d4e9b030.svg"
+  }
+]);
